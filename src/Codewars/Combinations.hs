58daebfe34bb0d955d7b65c4ef350467a8d903fe
Marek Mizier <marek.mizier@currency-one.com>
2019-04-21 00:30:41 +0200

    codewars: Combinations

---
+++ src/Codewars/Combinations.hs
@@ -0,0 +1,6 @@
+module Codewars.Combinations where
+import Data.List (tails)
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n xs = [ y:ys | y:xs' <- tails xs, ys <- combinations (n-1) xs']
